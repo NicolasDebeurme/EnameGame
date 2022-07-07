9a3c6929f5af11bc1ed17c63a272f4e1d30a7da3
Hakim <71723927+HakimDuparcq@users.noreply.github.com>
2022-07-07 12:56:26 +0200

    remove script

--- Assets/EnameGame/Scripts/LoadingManager/Classes/OnSceneUnloaded.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using UnityEngine.Events;
-using UnityEngine.SceneManagement;
-
-namespace LoadHandling
-{
-    [Serializable]
-    public class OnSceneUnloaded : UnityEvent<Scene> { };
-}
