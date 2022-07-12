bc2f3828a0fbdf0f2418e01513d55b81434c34b5
NicolasDebeurme <71765691+NicolasDebeurme@users.noreply.github.com>
2022-07-12 09:46:20 +0100

    ProjectClean

---
+++ Assets/ENAME_GAME/Scripts/Game/GameManager.cs
@@ -0,0 +1,53 @@
+using Niantic.ARDK;
+using Niantic.ARDK.LocationService;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using static Enums;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    //Runtime environment
+    public RuntimeEnvironment runtimeEnv;
+    public string TreeToLoad_Name ="DefaultTree";
+
+    //GamePlay
+    [NonSerialized]
+    public GameStateSystem _actualGameState = null;
+
+    public UI_Inventory uiInventory;
+
+    public bool HasSwap = false;
+    public bool HasDenounce = false;
+    public bool HasShoot = false;
+    //
+
+    #region Monobehaviour
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        _actualGameState = gameObject.AddComponent<GameStateSystem>();
+    }
+    #endregion
+
+    public void OnContinue()
+    {
+        GameStateSystem.Instance.GetState().NextState();
+    }
+    public void BroadcastNextState()
+    {
+        NetworkingManager.BroadCastNextState();
+    }
+    public void NetworkInitialized()
+    {
+    }
+
+}
